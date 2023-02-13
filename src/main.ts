20bc1167e491f29139a7fc4768063797727b6123
Nayla Gomes <nayla.cgs@gmail.com>
2023-02-13 10:30:54 -0300

    style: format file

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { prompt } from "enquirer";
+import { prompt } from 'enquirer'
 
 const getMangaName = async (): Promise<string> => {
   const result: { name: string } = await prompt({
-    type: "input",
-    name: "name",
-    message: "Digite o nome do mangá:",
-  });
+    type: 'input',
+    name: 'name',
+    message: 'Digite o nome do mangá:'
+  })
 
-  return result.name;
-};
+  return result.name
+}
 
-(async () => {
-  const mangaName = await getMangaName();
-  console.log(mangaName);
-})();
+await (async (): Promise<void> => {
+  const mangaName = await getMangaName()
+  console.log(mangaName)
+})()
