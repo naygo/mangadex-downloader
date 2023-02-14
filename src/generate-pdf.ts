ce7e1bab78d15f640c33f19a0900a1157550b06b
Nayla Gomes <nayla.cgs@gmail.com>
2023-02-14 14:18:30 -0300

    feat: add function that generate pdf

---
+++ src/generate-pdf.ts
@@ -0,0 +1,45 @@
+import 'dotenv/config'
+
+import { createWriteStream, unlinkSync } from 'fs'
+import { resolve } from 'path'
+import PDFDocument from 'pdfkit'
+import sizeOf from 'image-size'
+
+export async function createChapterPDF (
+  caminhoDasImagens: string[],
+  nomeManga: string,
+  volume: string
+): Promise<any> {
+  console.log('Criando arquivo PDF...')
+
+  const manga = new PDFDocument({
+    autoFirstPage: false,
+    size: [960, 1481],
+    compress: true
+  })
+
+  if (process.env.DOWNLOAD_FOLDER == null) throw new Error('Local de download não encontrado')
+
+  const nomeArquivo = `${nomeManga} - Vol. ${volume}.pdf`
+  const caminhoArquivo = resolve(process.env.DOWNLOAD_FOLDER, nomeArquivo)
+  manga.pipe(createWriteStream(caminhoArquivo))
+
+  for (const imagem of caminhoDasImagens) {
+    const dimensoes = sizeOf(imagem)
+    await manga
+      .addPage({ margin: 0, size: [dimensoes.width, dimensoes.height] })
+      .image(imagem, 0, 0, {
+        height: manga.page.height
+      })
+
+    unlinkSync(imagem)
+  }
+
+  await manga.end()
+  console.log('Arquivo PDF criado com sucesso! :D')
+
+  return {
+    nomeArquivo,
+    caminhoArquivo
+  }
+}
