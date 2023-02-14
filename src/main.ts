088a485fffc86701fbc3827421149df7ffee65e4
Nayla Gomes <nayla.cgs@gmail.com>
2023-02-14 14:04:06 -0300

    style: separate main file

--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,11 @@
-import { prompt } from 'enquirer'
-import { getMangaById, findMangasByName } from './service/mangadex/mangadex-client'
-
-import type { Manga } from './interfaces/manga'
-
 import { downloadManga } from './download-manga'
-
-import { showMangaInfo } from './utils/show-manga-info'
-import { getSelectedMangaInfo } from './list-mangas'
-import { readUserManga } from './read-user-manga'
-import { ChoiceEnum } from './enums/choice'
+import { getUserManga } from './manga-info'
 
 const main = async (): Promise<void> => {
-  try {
-    let mangaId = ''
+  const mangaInfo = await getUserManga()
+  console.log(mangaInfo)
 
-    while (mangaId === '' || mangaId !== 'cancel') {
-      let mangaInfo: Manga | null = null
-      const searchManga = await readUserManga()
-
-      if (searchManga.type === 'name') {
-        mangaInfo = await getMangaByName(searchManga.manga)
-      } else {
-        mangaInfo = (await getMangaById(searchManga.manga)).data
-      }
-
-      showMangaInfo(mangaInfo)
-
-      const confirmDownload = await confirmMangaSelection()
-      console.log(confirmDownload)
-      switch (confirmDownload) {
-        case ChoiceEnum.YES:
-          console.log('Baixando mangá...')
-          mangaId = mangaInfo.id
-          break
-        case ChoiceEnum.SEARCH_AGAIN:
-          console.log('Pesquisando outro mangá...')
-          mangaId = ''
-          break
-        case ChoiceEnum.CANCEL:
-          console.log('Cancelando...')
-          mangaId = 'cancel'
-          break
-      }
-    }
-
-    if (mangaId !== 'cancel') {
-      console.log('Busca cancelada')
-    } else {
-      downloadManga(mangaId)
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function getMangaByName (mangaName: string): Promise<Manga> {
-  const mangasFounded = await findMangasByName(mangaName)
-  const mangaInfo = await getSelectedMangaInfo(mangasFounded, mangaName)
-
-  if (mangaInfo == null) throw new Error('Manga não encontrado')
-
-  return mangaInfo
-}
-
-async function confirmMangaSelection (): Promise<ChoiceEnum> {
-  const { confirm }: { confirm: ChoiceEnum } = await prompt({
-    type: 'select',
-    name: 'confirm',
-    message: 'Deseja baixar o mangá?',
-    choices: [ChoiceEnum.YES, ChoiceEnum.SEARCH_AGAIN, ChoiceEnum.CANCEL]
-  })
-
-  return confirm
+  downloadManga()
 }
 
 void main()
---
+++ src/manga-info.ts
@@ -0,0 +1,71 @@
+import {
+  findMangasByName,
+  getMangaById
+} from './service/mangadex/mangadex-client'
+
+import type { Manga } from './interfaces/manga'
+
+import { showMangaInfo } from './utils/show-manga-info'
+import { readUserManga } from './read-user-manga'
+import { ChoiceEnum } from './enums/choice'
+import { getSelectedMangaInfo } from './list-mangas'
+import { prompt } from 'enquirer'
+
+export async function getUserManga (): Promise<Manga | null> {
+  try {
+    const mangaInfo = await getMangaInfo()
+    const confirmDownload = await confirmMangaSelection()
+
+    switch (confirmDownload) {
+      case ChoiceEnum.YES:
+        console.log('Baixando mangá...')
+        return mangaInfo
+      case ChoiceEnum.SEARCH_AGAIN:
+        console.log('Buscando novamente...')
+        await getUserManga()
+        break
+      case ChoiceEnum.CANCEL:
+        console.log('Cancelando...')
+        break
+    }
+
+    return null
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+async function getMangaInfo (): Promise<Manga | null> {
+  let mangaInfo: Manga | null = null
+  const searchManga = await readUserManga()
+
+  if (searchManga.type === 'name') {
+    mangaInfo = await getMangaByName(searchManga.manga)
+  } else {
+    mangaInfo = (await getMangaById(searchManga.manga)).data
+  }
+
+  showMangaInfo(mangaInfo)
+  return mangaInfo
+}
+
+async function getMangaByName (mangaName: string): Promise<Manga> {
+  const mangasFounded = await findMangasByName(mangaName)
+  const mangaInfo = await getSelectedMangaInfo(mangasFounded, mangaName)
+
+  if (mangaInfo == null) throw new Error('Manga não encontrado')
+
+  return mangaInfo
+}
+
+async function confirmMangaSelection (): Promise<ChoiceEnum> {
+  const { confirm }: { confirm: ChoiceEnum } = await prompt({
+    type: 'select',
+    name: 'confirm',
+    message: 'Deseja baixar o mangá?',
+    choices: [ChoiceEnum.YES, ChoiceEnum.SEARCH_AGAIN, ChoiceEnum.CANCEL]
+  })
+
+  return confirm
+}
