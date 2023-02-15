17af6daea9922daff3187e2522b730ed1ebd8978
Nayla Gomes <nayla.cgs@gmail.com>
2023-02-15 14:47:29 -0300

    style: delete unsed files

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,6 @@
 import { cli } from './cli'
 
 const main = async (): Promise<void> => {
-  /*
-  const mangaInfo = await getUserManga()
-  console.log(mangaInfo)
-
-  if (mangaInfo == null) {
-    console.log('Manga não encontrado')
-    return
-  }
-
-  // TODO - get volumes and chapters to download
-  downloadManga(mangaInfo.id)
-  // TODO - images to pdf
-  // TODO - pdf to MOBI
-  */
-
-  // Refactoring
   await cli()
 }
 
